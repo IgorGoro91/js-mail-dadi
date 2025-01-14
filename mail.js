c97bc574c5fd6e24a8523520b33aa4e4b48073be
IgorGoro91 <igorysc@gmail.com>
2025-01-14 17:51:58 +0100

    add:Mail

---
+++ mail.js
@@ -0,0 +1,30 @@
+
+
+/*Crea una lista di email di invitati ad una festa.
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+Non è consentito usare nessun metodo proprio degli array (come includes, per esempio). */
+
+
+
+
+const mailInvitati =["igor@mail.com", "max@mail.com", "kate@mail.com", "pippo@mail.com"] ; //mail invitati
+
+let mail =prompt( "inserisci la tua mail "); // richiesta mail
+let invitati ;
+
+for( let i = 0; i < mailInvitati.length ; i++){
+
+    if( invitati = mail * mailInvitati ){
+
+    } else if ( invitati ){
+
+        console.log( " Benvenuto alla festa")
+    }else {
+
+        console.log("Mi dispiace la sua meal non e nella lista")
+    }
+
+
+}
