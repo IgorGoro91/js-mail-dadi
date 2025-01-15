34c7b463614ac709c3eba7b67239aad2b91f339c
IgorGoro91 <igorysc@gmail.com>
2025-01-15 15:50:14 +0100

    add:correzione

--- mail.js
+++ mail.js
@@ -9,26 +9,32 @@ Non è consentito usare nessun metodo proprio degli array (come includes, per es
 
 
 
-const mailInvitati =["igor@mail.com", "max@mail.com", "kate@mail.com", "pippo@mail.com"] ; //mail invitati
+let mailInvitati =["igor@mail.com", "max@mail.com", "kate@mail.com", "pippo@mail.com"] ; //mail invitati
 
 
 let mail =prompt( "inserisci la tua mail "); // richiesta mail
 
- let risultato;
-for( let i = 0; i <= mailInvitati.length ; i++){
-    if(mail == mailInvitati[i]){
-        risultato = mail
-        console.log( `${risultato} Benvenuto alla festa`);
+ let risultato = false;
+
+for( let i = 0; i < mailInvitati.length ; i++){
+    
+    let singoloElemento = mailInvitati[i];
+
+    if( singoloElemento=== mail){
+        risultato = true
     }
-    if(risultato){
-        
+}
+
+    if( risultato=== true){
+        console.log(`${mail} Benvenuto nella festa`)
     }
+
     
     else{
         console.log("Mi dispiace la sua meal non e nella lista");
     }
     // INFO SE L'EMAIL E' STATA TROVATA O NO VIENE DA QUESTO IF
 
-}
+
 
 //QUI IL CONSOLE LOG
